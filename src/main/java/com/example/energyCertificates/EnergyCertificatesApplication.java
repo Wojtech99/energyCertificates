7ee9a68dac9e3152346ee0272dc30956cc1d42a5
Wojciech_N-ski <90407549+Wojtech99@users.noreply.github.com>
2024-05-16 20:19:31 +0200

    Initial commit

---
+++ src/main/java/com/example/energyCertificates/EnergyCertificatesApplication.java
@@ -0,0 +1,13 @@
+package com.example.energyCertificates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyCertificatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyCertificatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/energyCertificates/EnergyCertificatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.energyCertificates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyCertificatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
