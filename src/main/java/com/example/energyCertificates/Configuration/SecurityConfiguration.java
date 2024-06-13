ae338caea276fd93695753ac8602b41a8093eaf3
Wojciech_N-ski <90407549+Wojtech99@users.noreply.github.com>
2024-06-13 18:14:08 +0200

    Create security logic

--- src/main/java/com/example/energyCertificates/Configuration/SecurityConfiguration.java
+++ src/main/java/com/example/energyCertificates/Configuration/SecurityConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -12,8 +14,9 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(request -> request
-                .requestMatchers("/").permitAll()
-                .requestMatchers("/static/styles/**").permitAll()
+                .requestMatchers("/**").permitAll()
+                .requestMatchers("/static/userFront/**").permitAll()
+                .requestMatchers("/static/clientFront/**").permitAll()
                 .anyRequest().permitAll());
 
         httpSecurity.formLogin(login -> login.loginPage("/login")
@@ -25,5 +28,10 @@ public class SecurityConfiguration {
                 .logoutSuccessUrl("/")
         );
         return httpSecurity.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
--- src/main/java/com/example/energyCertificates/User/Service/UserService.java
+++ src/main/java/com/example/energyCertificates/User/Service/UserService.java
@@ -2,7 +2,11 @@ package com.example.energyCertificates.User.Service;
 
 import com.example.energyCertificates.User.Dtoes.UserCredentialsDto;
 import com.example.energyCertificates.User.Mappers.UserCredentialsMapper;
+import com.example.energyCertificates.User.User;
 import com.example.energyCertificates.User.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -10,13 +14,25 @@ import java.util.Optional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Optional<UserCredentialsDto> getUserCredentials(String email) {
-        return Optional.of(userRepository.findUserByEmail(email))
+        return Optional.of(userRepository.getUserByEmail(email))
                 .map(UserCredentialsMapper::map);
+    }
+
+    @Transactional
+    public void changePassword(String newPassword) {
+        String currentUserEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        User user = userRepository.getUserByEmail(currentUserEmail);
+        String passwordHash = passwordEncoder.encode(newPassword);
+
+        user.setPassword(passwordHash);
     }
 }
